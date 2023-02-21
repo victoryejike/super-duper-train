34d2d4ce57b3dc95498cb9c2c35e97a7c4b18962
Ejike Victory <victoryejike@gmail.com>
2023-02-21 21:43:01 +0100

    feat: setting up folder structure

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import { Inter } from "@next/font/google";
+import { Product, Footer, HeroBanner } from "@/components";
+import { client } from "@/lib/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div>banner</div>
-      <div>Best selling products</div>
-      <div>footer</div>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>Best selling products</h2>
+        <p>Speaker of many variation</p>
+      </div>
+      <div className="products-container">
+        <Product />
+      </div>
+      <Footer />
     </>
   );
 }
