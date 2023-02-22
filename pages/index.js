103cabd030234b64f05f36edae82d83d7cd17a7d
Ejike Victory <victoryejike@gmail.com>
2023-02-22 07:58:32 +0100

    feat: banner component done

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,28 @@ import { client } from "@/lib/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ products, bannerData }) {
   return (
     <>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best selling products</h2>
         <p>Speaker of many variation</p>
       </div>
       <div className="products-container">
-        <Product />
+        {products?.map((product) => product.name)}
       </div>
       <Footer />
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return { props: { products, bannerData } };
+};
