e89ad8804639d0048e09b09091751cee3372e9f2
Victory Ejike <victoryejike@gmail.com>
2023-02-25 22:24:58 +0100

    feat: creating react context for managing state of the app

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { AiOutlineShopping } from "react-icons/ai";
+
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+	return (
+		<div className="navbar-container">
+			<p className="logo">
+				<Link href="/">Headphones</Link>
+			</p>
+			<button className="cart-icon" type="button">
+				<AiOutlineShopping />
+				<span className="cart-item-qty">1</span>
+			</button>
+		</div>
+	);
 };
 
 export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { Layout } from "@/components";
 import "@/styles/globals.css";
+import { stateContext } from "@/context/stateContext";
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<stateContext>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</stateContext>
 	);
 }
